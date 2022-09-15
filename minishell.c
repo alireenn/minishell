951de1e853c78b1e8c85192b561d60f705f6dc45
alireenn <96998198+alireenn@users.noreply.github.com>
2022-09-15 17:43:21 +0200

    add process_input and init_mini

--- minishell.c
+++ minishell.c
@@ -12,6 +12,37 @@
 
 #include "incl/new_parser.h"
 
+void	process_input(t_mini *mini, char *input)
+{
+	if (input[0] == '\0')
+		return ;
+	add_history(input);
+	mini->tree = make_tree(input, &mini->cmd);
+	mini->commands = get_cmds(input, mini);
+	print_tree(&(mini->tree));
+	print_cmds(mini->commands, mini->cmd);
+	mini->res = execute(mini->tree, mini->commands, mini);
+	mini->cmd = 0;
+	free_cmds(mini->commands, mini->cmd);
+	free_tree(&(mini->tree));
+}
+
+void	init_mini(t_mini *mini, char **envp)
+{
+	mini->exit = 0;
+	init_env(mini, envp);
+	mini->secret = NULL;
+	init_secret_env(mini);
+	mini->pwd = malloc(sizeof(char) * PATH_MAX);
+	getcwd(mini->pwd, PATH_MAX);
+	mini->save_out = dup(STDOUT_FILENO);
+	mini->save_in = dup(STDIN_FILENO);
+	mini->tree = NULL;
+	mini->last = 0;
+	mini->res = 1;
+	mini->cmd = 0;
+}
+
 int	main(int argc, char **argv)
 {
 	char		*parsed;
@@ -34,6 +65,8 @@ int	main(int argc, char **argv)
 	// expand()
 	get_redirs(splitted, cmds, cmd);
 	int i = 0;
+	init_mini(mini, envp);
+	process_input(mini, prompt);
 	while (i < cmd)
 	{
 		printf("%s\n", splitted[i]);
