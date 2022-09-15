0166fdfb63f443ead54a40bc3ca1e13dcdc0615f
Giorgia Cucino <gcucino@student.42roma.it>
2022-09-15 14:48:14 +0200

    emergency

--- srcs/commands/redirections.c
+++ srcs/commands/redirections.c
@@ -6,13 +6,43 @@
 /*   By: gcucino <gcucino@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 16:20:10 by gcucino           #+#    #+#             */
-/*   Updated: 2022/09/13 18:43:31 by gcucino          ###   ########.fr       */
+/*   Updated: 2022/09/15 14:47:26 by gcucino          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/new_parser.h"
 
 // 1 -> '>'
+
+int	here_doc(char *end)
+{
+	pid_t	reader;
+	int		fd[2];
+	char	*tmp;
+
+	if (pipe(fd) == -1)
+		return (-1);
+	reader = fork();
+	if (reader == 0)
+	{
+		close(fd[0]);
+		tmp = readline("> ");
+		while (ft_strlen(tmp) != ft_strlen(end) || ft_strncmp(tmp, end, ft_strlen(end)) != 0)
+		{
+			write(fd[1], tmp, ft_strlen(tmp));
+			write(fd[1], "\n", 1);
+			free(tmp);
+			tmp = readline("> ");
+		}
+		exit(0);
+	}
+	else
+	{
+		close(fd[1]);
+		wait(NULL);
+		return (fd[0]);
+	}
+}
 
 char	*get_file_io(char **s, int r, int j, int *type)
 {
@@ -38,6 +68,29 @@ char	*get_file_io(char **s, int r, int j, int *type)
 	return (file);
 }
 
+int	get_red_io(t_command *cmd, char *filename, int type)
+{
+	mode_t	mode;
+
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	if (type == 1)
+		return (open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode));
+	else if (type == 2)
+	{
+		if (access(filename, F_OK) != 0)
+		{
+			// printf_fd(2,"minishell: %s: No such file or directory\n", filename);
+			// mini->last = 1;
+			cmd->res = 0;
+			return (-1);
+		}
+	}
+	else if (type == 3)
+		return (open(filename, O_WRONLY | O_CREAT | O_APPEND, mode));
+	else
+		return (here_doc(filename));
+}
+
 void	get_redirs(char **s, t_command **cmds, int cmd)
 {
 	int		i;
@@ -57,7 +110,7 @@ void	get_redirs(char **s, t_command **cmds, int cmd)
 				file = get_file_io(s, i, j, &type);
 				if (file == NULL)
 					printf("error");
-				get_red_io(cmds[i], type);
+				cmds[i]->fd_red[type % 2] = get_red_io(cmds[i], file, type);
 			}
 			j++;
 		}
